534befc33c48b33a2528ee6be7b0ed54e5ab6d64
Juan Felipe Mosquera Morales <juanfmx2@gmail.com>
2014-03-17 14:38:26 -0500

    Ajuste consulta de archivos completados

--- release/web_app/routes/index.js
+++ release/web_app/routes/index.js
@@ -1,8 +1,12 @@
 
+var fs = require('fs');
+
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-	res.render('index', { title: 'Express' });
+	var files = fs.readdirSync('./hadoop_data');
+	console.log(files);
+	res.render('index', { title: 'Consulta Grupo 5' },results:files);
 };
